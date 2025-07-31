ca767054870880b1f8cbc84f8d9908e5aa36260a
Anish-official2026 <anish001official@gmail.com>
2025-07-31 14:17:37 +0530

    Create Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,11 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  window.location.href = "/login";
+});
+
+document.getElementById("signup-btn").addEventListener("click", function () {
+  window.location.href = "/signup";
+});
+
+document.getElementById("upload-btn").addEventListener("click", function () {
+  window.location.href = "/upload";
+});
